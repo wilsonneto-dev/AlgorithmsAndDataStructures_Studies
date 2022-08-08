049fa96a8535b5e352fcc3fec942943f07680355
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-08-08 18:17:49 -0300

    Update RotateArray.cs

--- TwoPointers/RotateArray.cs
+++ TwoPointers/RotateArray.cs
@@ -1,11 +1,11 @@
-/*
+
     public void Rotate(int[] nums, int k) {
         var rotatedArray = new int[nums.Length];
         for(int i = 0; i < nums.Length; i++)
              rotatedArray[(i + k) % nums.Length] = nums[i];
         return rotatedArray;
     }
-*/
+
 /*
     public void Rotate(int[] nums, int k) {
         for(int i = 0; i < k; i++)
@@ -27,5 +27,30 @@ public class Solution {
                 nums[j] = previous;
                 previous = hold;
             }            
+    }
+}
+
+public class Solution {
+    public void Rotate(int[] nums, int k) {
+        if(nums is null || nums.Length < 2 || k == 0) 
+            return;
+        
+        var rotationCounter = k % nums.Length;
+        reverse(nums);
+        reverse(nums, rotationCounter);
+        reverse(nums, 0, rotationCounter - 1);
+    }
+    
+    private void reverse(int[] nums, int start = 0, int end = -1)
+    {
+        if(end == -1) end = nums.Length - 1;
+        int left = start, right = end;
+        while(left < right)
+        {
+            var temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++; right--;
+        }
     }
 }
