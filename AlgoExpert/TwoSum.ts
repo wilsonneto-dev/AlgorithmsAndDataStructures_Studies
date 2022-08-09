b4d37818f1a021067808fca086d0fee08f8aa006
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-08-08 22:16:38 -0300

    Update TwoSum.ts

--- AlgoExpert/TwoSum.ts
+++ AlgoExpert/TwoSum.ts
@@ -1,6 +1,6 @@
 // https://www.algoexpert.io/questions/two-number-sum
 
-// first approach => O(N) / O(N) => 18 minutes
+// first approach => O(N) better! / O(N) => 18 minutes
 
 export function twoNumberSum(array: number[], targetSum: number) {
   let missingValues: { [key: number]: boolean} = {};
@@ -11,5 +11,19 @@ export function twoNumberSum(array: number[], targetSum: number) {
       return [array[num], missingValue];
     missingValues[array[num]] = true;
   }
+  return [];
+}
+
+// second approach after watching the solution -> time O(N log N) / space O(1) better!
+export function twoNumberSum(array: number[], targetSum: number) {
+    array.sort((x, y) => x - y);
+    let left = 0, right = array.length - 1;
+    while(left < right)
+    {
+      const sum = array[left] + array[right];
+      if(sum == targetSum) return [array[left] , array[right]];
+      if(sum < targetSum) left++;
+      else right--;
+    }
   return [];
 }
