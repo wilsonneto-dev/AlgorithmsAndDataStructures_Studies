229c2fdd15e1ce6e1464a8345b730249c2eba256
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-08-08 21:25:58 -0300

    Update MoveZeros.cs

--- TwoPointers/MoveZeros.cs
+++ TwoPointers/MoveZeros.cs
@@ -31,3 +31,22 @@ public class Solution {
             nums[left++] = 0;
     }
 }
+
+// more one attempt optimizing the writes in the arfray after watching the solution
+// the above solutions will write a lot of zeros in the array at the end, I can just swap and write in the best case only one time
+
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        for(int idx = 0, lastNonZero = 0; idx < nums.Length; idx++)
+            if(nums[idx] != 0) 
+            {
+                if(idx != lastNonZero)
+                {
+                    var hold = nums[lastNonZero];
+                    nums[lastNonZero] = nums[idx];
+                    nums[idx] = hold;                    
+                }
+                lastNonZero++;
+            }
+    }
+}
