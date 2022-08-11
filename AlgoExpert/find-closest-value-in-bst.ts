6872cb05c1f843980d89823e6330714ad19fd863
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-08-11 19:49:49 -0300

    Update find-closest-value-in-bst.ts

--- AlgoExpert/find-closest-value-in-bst.ts
+++ AlgoExpert/find-closest-value-in-bst.ts
@@ -1,6 +1,6 @@
 // https://www.algoexpert.io/questions/find-closest-value-in-bst
 
-// 22/08/11 -> t = O(n) / s = O(1) -> 40 minutes
+// 22/08/11 -> t = O(k) / s = O(k) -> 40 minutes
 export function findClosestValueInBst(tree: BST, target: number, closest: number | null = null): number|null  {
   if(tree.value == target) return tree.value;
   
@@ -10,4 +10,18 @@ export function findClosestValueInBst(tree: BST, target: number, closest: number
   const next = (target < tree.value) ? tree.left : tree.right;
   if(next == null) return closest;
   return findClosestValueInBst(next, target, closest);
+}
+
+// t = O(K) / s = O(1)
+export function findClosestValueInBst(tree: BST, target: number): number|null  {
+  let closest: number | null = null, 
+    current: BST | null = tree;
+  while(current != null)
+  {
+    if(current.value == target) return current.value;
+    if(closest == null || Math.abs(target - current.value) < Math.abs(target - closest))
+      closest = current.value;
+    current = (target < current.value) ? current.left : current.right;
+  }
+  return closest;
 }
