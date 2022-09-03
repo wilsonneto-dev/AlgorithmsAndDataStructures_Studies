3187d03d31ee5ca2b3a54029694dfe6d2b5efec9
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-09-03 16:00:37 -0300

    Update ReverseWordsInString.cs

--- TwoPointers/ReverseWordsInString.cs
+++ TwoPointers/ReverseWordsInString.cs
@@ -32,3 +32,41 @@ public class Solution {
         return new string(arr);
     }
 }
+
+// 22/09/03 (random review) -> 33 minutes -> time O(n) / space O(n)
+public class Solution {
+    public string ReverseWords(string s) {
+        if(s.Length == 0) return s;
+        string result = "";
+        int wordStartIndex = 0, wordEndIndex = 0;
+        while(wordEndIndex < s.Length)
+        {
+            if(wordEndIndex == s.Length - 1 || s[wordEndIndex + 1] == ' ')
+            {
+                result += ReverseStringPartial(wordStartIndex, wordEndIndex, s);
+                if(wordEndIndex < s.Length - 1)
+                    result += " ";
+                wordStartIndex = wordEndIndex += 2;
+                continue;
+            }
+            wordEndIndex += 1; 
+        }
+        return result;
+    }
+    
+    private string ReverseStringPartial(int initialIndex, int finalIndex, string str)
+    {
+        char[] result = new char[finalIndex - initialIndex + 1];
+        int offset = initialIndex;
+        
+        while(initialIndex <= finalIndex)
+        {
+            result[initialIndex - offset] = str[finalIndex];
+            result[finalIndex - offset] = str[initialIndex];
+            finalIndex--;
+            initialIndex++;
+        }
+        
+        return new string(result);
+    }
+}
