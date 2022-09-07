c1e842409c021e756e44c1e6919a362c8e8ea0db
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-09-06 21:50:57 -0300

    Update node-depths.js

--- AlgoExpert/node-depths.js
+++ AlgoExpert/node-depths.js
@@ -16,3 +16,37 @@ class BinaryTree {
 
 // Do not edit the line below.
 exports.nodeDepths = nodeDepths;
+
+// ----
+
+// in a bfs approach:
+
+function nodeDepths(root = null) {
+  if(root == null)
+    return 0;
+  const queue = [];
+  let sum = 0;
+  queue.push([root, 0]);
+  
+  while(queue.length > 0)
+  {
+    const [node, level] = queue.shift();
+    sum += level;
+    if(node.left !== null) queue.push([node.left, level+1]);
+    if(node.right !== null) queue.push([node.right, level+1]);
+  }
+
+  return sum;
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Do not edit the line below.
+exports.nodeDepths = nodeDepths;
