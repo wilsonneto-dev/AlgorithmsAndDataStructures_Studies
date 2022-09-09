ffe6f80b885bcdf52d4e4279cfd528c80791e462
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-09-09 15:53:22 -0300

    Update merge-two-binary-trees.cs

--- Practicing/merge-two-binary-trees.cs
+++ Practicing/merge-two-binary-trees.cs
@@ -27,3 +27,21 @@ public class Solution {
         ); 
     }
 }
+
+// refactoring
+public class Solution {
+    public TreeNode MergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 is null && root2 is null)
+            return null;
+        if(root1 is null)
+            return root2;
+        if(root2 is null)
+            return root1;
+        
+        return new TreeNode(
+            root1.val + root2.val,
+            MergeTrees(root1.left, root2.left),
+            MergeTrees(root1.right, root2.right)
+        ); 
+    }
+}
